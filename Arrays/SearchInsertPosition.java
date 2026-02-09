65acc2aa9362103375d10d1b84f1e7e0b3a9fb3d
rushi-dev24 <rushi-dev24@outlook.com>
2026-02-09 10:59:14 +0530

    Implementation of searchInsert
    Refactor searchInsert method to use linear search instead of binary search.

--- Arrays/SearchInsertPosition.java
+++ Arrays/SearchInsertPosition.java
@@ -1,17 +1,13 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int start=0,end =nums.length-1,mid=0;
-        while(start<=end){
-            mid=start + (end - start)/2;
-            if(target>nums[mid]){
-                start=mid+1;
-            }
-            else if(target<nums[mid]){
-                end=mid-1;
-            } else{
-                return mid;
+        int res=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
+                return i;
+            }else if(nums[i]<target){
+                res=i;
             }
         }
-        return start;
+        return res+1;
     }
 }
