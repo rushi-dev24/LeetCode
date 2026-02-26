9c6ca01cc185466fad122a348e4453d03291b2b7
rushi-dev24 <rushi-dev24@outlook.com>
2026-02-26 22:33:21 +0530

    Create ValidParlindrome.java

---
+++ Two Pointer/ValidParlindrome.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int n=s.length(),i=0,j=n-1;
+        while(i<=j){
+            if(s.charAt(i++) != s.charAt(j--)) return false;
+        }
+        return true;
+    }
+}
